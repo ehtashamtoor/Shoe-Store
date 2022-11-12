4bcf683ac856cd4e02333bae3dc5758491f96578
EHTASHAM TOOR <ehtashamtoor50@gmail.com>
2022-11-13 00:40:21 +0500

    Designed Generate Bill component for invoice

--- src/components/GenerateBill/GenerateBill.js
+++ src/components/GenerateBill/GenerateBill.js
@@ -1,9 +1,58 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const GenerateBill = () => {
-  return (
-    <div>GenerateBill</div>
-  )
+
+    const { All_items } = useSelector(state => state);
+
+
+    return (
+        <div className='container'>
+            <div className="row">
+                <div className="col-12 invoiceTitle">
+                    <h2>Invoice</h2>
+                    <p>Thanks for Shopping With Us!🤩</p>
+                </div>
+                <div className="col-md-6 col-sm-12 invoicewrapper">
+                    <table className='invoice'>
+                        <thead>
+                            <th>Image</th>
+                            <th>Item</th>
+                            <th>Qty</th>
+                            <th>Price</th>
+                        </thead>
+                        <tbody>
+
+                            {
+                                All_items.added_items.map((item, index) => {
+                                    return <tr key={index}>
+                                        <td><img src={item.img} alt=".." className='img-fluid' /></td>
+                                        <td>{item.name}</td>
+                                        <td>{item.quantity}</td>
+                                        <td><strong>Rs:{item.price}</strong></td>
+                                    </tr>
+                                })
+                            }
+                            <tr>
+                                <td colSpan={3} className="total">
+                                    <strong>Total:</strong>
+                                </td>
+                                <td colSpan={3}>
+                                    <strong>{All_items.total}</strong>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colSpan={4}>
+                                    <button className="btn btn-info PayBtn">Pay Bill</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default GenerateBill
