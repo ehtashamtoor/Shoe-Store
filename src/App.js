eca0b11271652dcc917568014a03892ccf37fda2
EHTASHAM TOOR <ehtashamtoor50@gmail.com>
2022-11-14 20:14:59 +0500

    home

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<Products />} />
           <Route path='/about' element={<About />} />
           <Route path='/products' element={<Products />} />
           <Route path='/products/:productId' element={<ProductItem />} />
